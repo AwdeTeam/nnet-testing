94be2904d99d43019676f031176c3e3839dba3db
AwdeDarkar <darkardengeno@gmail.com>
2016-02-05 20:28:03 -0500

    Final Commit for 2 5 2016

--- NNet.py
+++ NNet.py
@@ -20,7 +20,7 @@ class NeuralNetwork():
 	outputs = 0
 	
 	# construction
-	def __init__(self,inpNum,hiddenLayerNum,hiddenLayerNeurons,outNum):
+	def __init__(self, inpNum, hiddenLayerNum, hiddenLayerNeurons, outNum):
 		print "Network shape initialized: " + str(inpNum) + " inputs, " + str(hiddenLayerNum) + " hidden layers, " + str(hiddenLayerNeurons) + " neurons per hidden layer, " + str(outNum) + " outputs"
 		self.inputs = inpNum
 		self.layers = hiddenLayerNum
